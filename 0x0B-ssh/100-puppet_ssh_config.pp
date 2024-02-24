b62f8fbe0ee45b3ce37da41fc60322a7a7444763
EsraaGD <isra.hatim2@gmail.com>
2024-02-24 11:19:25 -0800

    T4 fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,22 @@
-# Define SSH client configuration file resource
-file { 'ubuntu/.ssh/config':
-  ensure  => file,
-  owner   => 'ubuntu',
-  mode    => '0644',
-  content => "
-    Host 512587-web-01
-        HostName '54.144.134.136'
-        User ubuntu
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-  ",
+# make changes to our config file to allow
+# configured to use the private key ~/.ssh/school
+# configured to refuse to authenticate using a password
+
+include stdlib
+
+# Ensure the SSH private key is added to the SSH config file
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => 'IdentityFile ~/.ssh/school',
+  match              => '^IdentityFile ~/.ssh/id_rsa',
+  replace            => false,
+}
+
+# Ensure password authentication is disabled in the SSH config file
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => 'PasswordAuthentication no',
+  match              => '^PasswordAuthentication yes',
+  replace            => false,
 }
 
