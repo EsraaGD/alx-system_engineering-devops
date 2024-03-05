6a3656f8c3a9141b569a16a3c3234214cc199c2a
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-05 20:36:19 +0300

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
 #configure web-02 to be identical to web-01
+
+exec { 'update':
+  command  => 'sudo apt -y update',
+  provider => 'shell',
+  before   => Exec['nginx'],
+}
+
+exec { 'nginx':
+  command  => 'sudo apt -y install nginx',
+  provider => 'shell',
+  before   => Exec['config'],
+}
+
+exec { 'config':
+  command  => 'sudo sed -i s@"http {"@"http {\n\tadd_header X-Served-By \$HOSTNAME;"@g /etc/nginx/nginx.conf',
+  provider => 'shell',
+  before   => Exec['restart'],
+}
+
+exec { 'restart':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
+}
