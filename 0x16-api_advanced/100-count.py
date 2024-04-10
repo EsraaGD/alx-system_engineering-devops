ca4c4e6438d11f8709788abaf729ca7c26a1eb6c
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-04-10 18:39:09 +0300

    All tasks

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""100-count module"""
+import requests
+
+
+def count_words(subreddit, word_list=[], word_count={}, after=None):
+    """
+    Queries the Reddit API recursively
+
+    Returns:
+        Prints a sorted count of given keywords
+        None if an invalid subreddit is given
+    """
+    URL = 'https://www.reddit.com/r/'
+    res = requests.get('{}{}/hot.json'.
+                       format(URL, subreddit),
+                       params={'after': after},
+                       headers={'User-Agent': 'ALX-User-Agent'},
+                       allow_redirects=False)
+
+    if res.status_code >= 300:
+        return None
+
+    list = hot_list + [child.get('data').get('title')
+                       for child in res.json().get('data').get('children')]
+
+    data = res.json()
+    if not data.get('data').get('after'):
+        return list
+
+    return recurse(subreddit, list, data.get('data').get('count'),
+                   data.get('data').get('after'))
